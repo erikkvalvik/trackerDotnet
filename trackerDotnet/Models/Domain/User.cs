f4d1e9c7c1f5e224b2f479a26d71db58eeda944b
erikkvalvik <erik.kvalvik@gmail.com>
2022-04-02 17:28:02 +0200

    domain

---
+++ trackerDotnet/Models/Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace trackerDotnet.Models.Domain
+{
+    /// <summary>
+    /// Domain Class for User
+    /// </summary>
+    [Table("User")]
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        //public ICollection<Sighting> Sightings { get; set; }
+    }
+}
